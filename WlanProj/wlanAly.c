8d62cf9b0c861c82bfcb6f27e9fbb22513c91ac5
Liyuan Liu <tenderlili@gmail.com>
2022-04-15 16:06:01 +0300

    Enabled network type in ssid, dBm format in Ap signal and decanding ap by signal strength.

--- WlanProj/wlanAly.c
+++ WlanProj/wlanAly.c
@@ -4,8 +4,6 @@
 #include <string.h>
 #include "wlanprj.h"
 
-
-
 void wlan_analyse(FILE* fp) {
 	int ssidNum = wlan_count(fp);
 	ssid* ssid_list = (ssid*)malloc(sizeof(ssid) * ssidNum);
@@ -23,11 +21,20 @@ void wlan_analyse(FILE* fp) {
 			strcpy(ssid_list[i].ssidnm,read_value(buf));
 			ssid_list[i].num_ap = j;	
 		}
+		else if ((lc >= 4) && (tmp == NET)) {
+			strcpy(ssid_list[i].ssid_net, read_value(buf));
+		}
+		else if ((lc >= 4) && (tmp == AUTH)) {
+			strcpy(ssid_list[i].ssid_aut, read_value(buf));
+		}
+		else if ((lc >= 4) && (tmp == ENCRYP)) {
+			strcpy(ssid_list[i].ssid_encr, read_value(buf));
+		}
 		else if ((lc >= 4) && (tmp == BSSID)) {
 			strcpy(ssid_list[i].aplist[j].apmac,read_value(buf));
 		}
 		else if ((lc >= 4) && (tmp == SIG)) {
-			ssid_list[i].aplist[j].signal_strength = atoi(read_value(buf));
+			ssid_list[i].aplist[j].signal_strength = dBm_calculation(atoi(read_value(buf)));
 		}
 		else if ((lc >= 4) && (tmp == CH)) {
 			ssid_list[i].aplist[j].channel = atoi(read_value(buf));
@@ -39,7 +46,6 @@ void wlan_analyse(FILE* fp) {
 	show_wlan(ssid_list, ssidNum);
 	free(ssid_list);
 }
-
 int wlan_count(FILE* fp) {
 	char line[MAXCHAR];
 	int c = 0;
@@ -54,7 +60,6 @@ int wlan_count(FILE* fp) {
 	rewind(fp);
 	return n;
 }
-
 char* read_value(char* buf) {
 	char value[30];
 	char* v = value;
@@ -87,18 +92,41 @@ void show_wlan(ssid* list, int ssidc) {
 	printf("\n");
 	for (int i = 0; i < ssidc; i++) {
 		printf("ESSID: %s", list[i].ssidnm);
+		printf("\n\tNetwork type: %s", list[i].ssid_net);
+		printf("\n\tAuthentication: %s", list[i].ssid_aut);
+		printf("\n\tEncryption: %s\n", list[i].ssid_encr);
 		printf("\n");
+		sort_by_signal(list[i].aplist[list[i].num_ap+1].signal_strength, list[i].num_ap + 1);
 		for (int j = 0; j < (list[i].num_ap + 1); j++) {
-			printf("AP %d MAC: %s  Channel: %d, Signal: %d%c",j + 1, list[i].aplist[j].apmac, list[i].aplist[j].channel, list[i].aplist[j].signal_strength, '%');
+			printf("\tAP %d MAC: %s  Channel: %d, Signal: %ddBm",j + 1, list[i].aplist[j].apmac, list[i].aplist[j].channel, list[i].aplist[j].signal_strength);
 		}
 		printf("\n\n");
 	}
 }
-
 char* takeawaynewline(char* value) {
 	int len = strlen(value);
 	if (value[len - 1] == '\n') {
 		value[len - 1] = 0;
 	}
 	return value;
+}
+int dBm_calculation(int sig) {
+	int dBm = (sig / 2) - 100;
+	return dBm;
+}
+
+void sort_by_signal(int number[], int n) {
+	int tmp;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = i + 1; j < n; ++j)
+		{
+			if (number[i] < number[j])
+			{
+				tmp = number[i];
+				number[i] = number[j];
+				number[j] = tmp;
+			}
+		}
+	}
 }
--- WlanProj/wlanprj.h
+++ WlanProj/wlanprj.h
@@ -12,6 +12,9 @@ typedef struct {
 
 typedef struct {
 	char ssidnm[40];//name of WLAN(SSID)
+	char ssid_net[30];
+	char ssid_aut[30];
+	char ssid_encr[10];
 	int num_ap;//number of aps
     bssid aplist[20];//a list of APs(dynamics)
 }ssid;
@@ -19,9 +22,9 @@ typedef struct {
 
 void wlan_analyse(FILE* fp);
 int wlan_count(FILE*);
-//int wlan_number(char*);
 char* read_value(char*);
 enum LINE_TYPE what_line(char*);
 void show_wlan(ssid*, int);
 char* takeawaynewline(char*);
-
+int dBm_calculation(int);
+void sort_by_signal(int number[], int n);
